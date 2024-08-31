edbc3d479fb79d24a6783246ce158a6369abb790
johnilizy17 <johnabrahamtosin@gmail.com>
2024-08-31 15:05:05 +0300

    database connection

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const CONNECTION_STRING = "mongodb://127.0.0.1:27017";
+// akintayoolufunmilola1
+// D42im8hVPmKweKKW
+const CONNECTION_STRING = "mongodb+srv://akintayoolufunmilola1:D42im8hVPmKweKKW@cluster0.ng3gq.mongodb.net/";
 const path = require('path');
 const routes = require('./routes');
 const bodyparser = require('body-parser');
